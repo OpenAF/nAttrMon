d76a56886f2d00b9cad75d1a82c879de8b1f5fe2
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2018-02-01 03:51:50 +0000

    nOutput_EmailWarnings rewrite

--- config/objects/nOutput_EmailWarnings.js
+++ config/objects/nOutput_EmailWarnings.js
@@ -21,6 +21,10 @@ var nOutput_EmailWarnings = function(aMap) {
 	this.warnTypes = (isUnDef(aMap.warnTypes)) ? [ "High" ] : aMap.warnTypes;
 	this.tls = aMap.tls;
 	this.debug = aMap.debug;
+	this.include = aMap.include;
+	this.exclude = aMap.exclude;
+
+	this.lock = false;
 
 	this.first = (isUnDef(aMap.dontAvoidStartEmails)) ? true : aMap.dontAvoidStartEmails;
 
@@ -34,7 +38,6 @@ nOutput_EmailWarnings.prototype.output = function (scope, args, meta) {
 		throw "nOutput_EmailWarnings only supports chSubscribe: nattrmon::warnings";
 
 	var owarns = scope.getWarnings(false);
-	var instanceId = sha1(meta.aName);
 
 	// Avoids the first email right out of starting  
 	if (this.first) {
@@ -42,45 +45,85 @@ nOutput_EmailWarnings.prototype.output = function (scope, args, meta) {
 		return;
 	}
 
-	var email = new Email(this.mailserver, this.from, (isDef(this.credentials) && !this.tls), this.tls, true);
-	if (isDef(this.port)) email.setPort(this.port);
-	if (isDef(this.credentials)) {
-		email.setCredentials(this.credentials.user, this.credentials.password);
+	var ws = (isArray(args.v) ? args.v : [ args.v ]);
+	var email, count = 0, data, doIt = true;
+
+	this.instanceId = sha1(meta.aName);
+
+	var shouldEmail = false;
+	for(var wsi in ws) {
+		var shouldEval = true;
+		if (isDef(this.include) && isArray(this.include) && this.include.indexOf(ws[wsi].title) < 0) shouldEval = false;
+		if (isDef(this.exclude) && isArray(this.exclude) && this.exclude.indexOf(ws[wsi].title) >= 0) shouldEval = false;
+		if (shouldEval && !nattrmon.isNotified(ws[wsi].title, this.instanceId) && this.warnTypes.indexOf(ws[wsi].level) >= 0)
+			shouldEmail = true;
 	}
-	if (isDef(this.debug) && this.debug) email.getEmailObj().setDebug(true);
 
-	var count = 0;
-	var mhash = this.aSubject + this.from.toSource();
-
-	var part = "<h2>" + this.subject + "</h2>";
-	for (var i in {
-			"High"  : 1,
-			"Medium": 2,
-			"Low"   : 3,
-			"Info"  : 4
-		}) {
-		part += "<h3>" + i + "</h3><table>";
-		for (var j in owarns[i]) {
-			if (!nattrmon.isNotified(owarns[i][j].title, instanceId) && 
-			    this.warnTypes.indexOf(owarns[i][j].level) >= 0) {
-				
-				var color = "";
-
-				if (i == 'High')   color = "red";
-				if (i == 'Medium') color = "yellow";
-
-				part = part + "<tr><td bgcolor=\"" + color + "\"><b>" + owarns[i][j].title + "</b></td><td><i>" + owarns[i][j].description + "</i></td><td><small><b>Updated</b>:<br>" + owarns[i][j].lastupdate + "</small></td><td><small><b>Created:</b><br>" + owarns[i][j].createdate + "</small></td></tr>";
-				mhash += owarns[i][j].title + owarns[i][j].description;
-
-				nattrmon.setNotified(owarns[i][j].title, instanceId);
-				count++;
+	if (shouldEmail) {
+		sync(() => {
+			if (this.lock == false) {
+				this.lock = true;
+				doIt = true;
+			} else {
+				doIt = false;
 			}
+		}, this.lock);
+
+		if (doIt) {
+			email = new Email(this.mailserver, this.from, (isDef(this.credentials) && !this.tls), this.tls, true);
+			if (isDef(this.port)) email.setPort(this.port);
+			if (isDef(this.credentials)) {
+				email.setCredentials(this.credentials.user, this.credentials.password);
+			}
+			if (isDef(this.debug) && this.debug) email.getEmailObj().setDebug(true);
+
+			data = {
+				subject: this.subject,
+				warns: []
+			};
+
+			for (var i in {
+					"High"  : 1,
+					"Medium": 2,
+					"Low"   : 3,
+					"Info"  : 4
+				}) {
+				
+				var list = [];
+
+				for (var j in owarns[i]) {
+					var shouldEval = true;
+					if (isDef(this.include) && isArray(this.include) && this.include.indexOf(owarns[i][j].title) < 0) shouldEval = false;
+					if (isDef(this.exclude) && isArray(this.exclude) && this.exclude.indexOf(owarns[i][j].title) >= 0) shouldEval = false;
+
+					if (shouldEval) {
+						list.push(owarns[i][j]);
+
+						if (!nattrmon.isNotified(owarns[i][j].title, this.instanceId) && 
+							this.warnTypes.indexOf(owarns[i][j].level) >= 0)
+								nattrmon.setNotified(owarns[i][j].title, this.instanceId);
+
+						count++;
+					}
+				}
+
+				data.warns.push({
+					level: i,
+					list: list
+				});
+			}
+
+			sync(() => {
+				if (this.lock == true) this.lock = false;
+			}, this.lock);
+		} else {
+			return;
 		}
-		part += "</table>";
 	}
 
 	if (count == 0) return;
-	var message = templify(nOutput_EmailWarnings.htmlTemplate, { part: part });
+	email.embedFile(nattrmon.configPath + "/objects.assets/noutputemailwarnings/logo.png", "logo");
+	var message = nOutput_EmailWarnings.htmlTemplate("nOutput_EmailWarnings", data);
 
 	email.setHTML(message);
 	try {	
@@ -94,31 +137,6 @@ nOutput_EmailWarnings.prototype.output = function (scope, args, meta) {
 	}
 };
 
-nOutput_EmailWarnings.htmlTemplate = "<html><style type=\"text/css\">\
-body {background-color: #ffffff; color: #000000; }\
-body, td, th, h1, h2 {font-family: 'Segoe UI Light','Segoe UI',-apple-system-font,'Lucida Grande',Verdana,Arial,Helvetica,serif ; font-style: normal; }\
-pre {margin: 0px; font-family: monospace;}\
-a:link    {color: #0000AA; text-decoration: none; background-color: #ffffff; }\
-a:visited {color: #0000AA;  text-decoration: none; background-color: #ffffff; }\
-a:hover {text-decoration: underline;}\
-table {border-collapse: collapse; margin-left: 25px; border: 0px }\
-th { text-align: center !important; border: 0px }\
-td, th { border: 0px; font-family: -apple-system-font, sans-serif; font-size: 85%; vertical-align: baseline; padding-left: 10px; padding-right: 10px; }\
-h1 {font-size: 150%; }\
-h2 {font-size: 125%; }\
-p {font-weight: normal; font-size: 100%; font-family: 'Segoe UI Light','Segoe UI',-apple-system-font,'Lucida Grande',Verdana,Arial,Helvetica,serif; font-style: normal; color: #999999; }\
-ul {font-size: 90%;}\
-h3 {text-align: left; font-size: 100%; color: #999999; border-bottom: 1px dotted #ffa500; }\
-.p {text-align: left;}\
-.b { font-style: normal; color: #999999; }\
-.e {background-color: #ffffff; font-weight: bold; color: #000000; text-align: right; padding-left: 30px; }\
-.h {background-color: #9999cc; font-weight: bold; color: #000000;}\
-.v {background-color: #ffffff; color: #999999; font-family: 'Segoe UI Light','Segoe UI',-apple-system-font,'Lucida Grande',Verdana,Arial,Helvetica,serif; font-weight: normal;  text-align: left; padding-top: 15px; font-size: 115%; border-bottom: 1px solid #ffa500; }\
-.vr {background-color: #cccccc; text-align: right; color: #000000;}\
-img {float: right; border: 0px;}\
-hr {background-color: #cccccc; border: 0px; height: 1px; color: #000000;}\
-.pagelist  { color: #999999;}\
-a.pagelist { color: #990000;}\
-</style><body>\
-{{{part}}}\
-</body></html>";
+nOutput_EmailWarnings.htmlTemplate = ow.template.loadHBSs({
+	"nOutput_EmailWarnings": nattrmon.configPath + "/objects.assets/noutputemailwarnings/warningEmailTemplate.hbs"
+});
