f3bbdf317416291f1caa7f4a28dec6e089739fd5
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2018-02-25 06:34:57 +0000

    improvements to nOutput_Log and nOutput_Stdout

--- config/outputs.disabled/4.logFile.js
+++
@@ -1,8 +0,0 @@
-nattrmon.addOutput(
-	{ "name"          : "Output log file",
-      "timeInterval"  : 500,
-      "waitForFinish" : true,
-      "onlyOnEvent"   : true,
-    },
-	new nOutput_Stdout()
-);
