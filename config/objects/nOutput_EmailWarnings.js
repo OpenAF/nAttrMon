0d4164026557266199160d1a42828c6deeb7e48c
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2018-06-19 02:47:25 +0100

    issue #62 + #63

--- config/objects/nOutput_EmailWarnings.js
+++ config/objects/nOutput_EmailWarnings.js
@@ -20,6 +20,8 @@ var nOutput_EmailWarnings = function(aMap) {
 	this.credentials = (isUnDef(aMap.credentials)) ? void 0 : aMap.credentials;
 	this.warnTypes = (isUnDef(aMap.warnTypes)) ? [ "High" ] : aMap.warnTypes;
 	this.descriptionLimit = _$(aMap.descriptionLimit).isNumber().default(-1);
+	this.groupByTitleSimilarity = _$(aMap.groupByTitleSimilarity).isNumber().default(void 0);
+	this.alertUntilBySimilarity = _$(aMap.alertUntilBySimilarity).isNumber().default(void 0);
 	this.tls = aMap.tls;
 	this.debug = aMap.debug;
 	this.include = aMap.include;
@@ -62,10 +64,11 @@ nOutput_EmailWarnings.prototype.output = function (scope, args, meta) {
 		return;
 	}
 
-	var ws = (isArray(args.v) ? args.v : [ args.v ]);
-	var email, count = 0, data, doIt = true;
-
 	this.instanceId = sha1(meta.aName);
+	var ws = (isArray(args.v) ? args.v : [ args.v ]);
+
+	var ws = nattrmon.getWarnings(true).getCh().getAll();
+	var email, count = 0, data, doIt = true;
 
 	var shouldEmail = false;
 	for(var wsi in ws) {
@@ -133,14 +136,39 @@ nOutput_EmailWarnings.prototype.output = function (scope, args, meta) {
 							if (isDef(this.descriptionLimit) && this.descriptionLimit > 0) {
 								owarns[i][j].description = owarns[i][j].description.substr(0, this.descriptionLimit - 1) + "...";
 							}
-							
-							list.push(owarns[i][j]);
+	
+							if (isDef(this.groupByTitleSimilarity) && this.groupByTitleSimilarity > 0) {
+								var parent = this;
+								var resDupls = $from($from(owarns[i]).select((r) => {
+									return { 
+										t: r.title, 
+										d: ow.format.string.distance(owarns[i][j].title, r.title),
+										n: nattrmon.isNotified(r.title, parent.instanceId)
+									};
+								}))
+								.less("d", (owarns[i][j].title.length * (this.groupByTitleSimilarity/100)))
+								.notEquals("t", owarns[i][j].title)
+								.select();
+
+								if (resDupls.length > 0) owarns[i][j].extraMessage = "(plus " + resDupls.length + " equivalent warnings)";
+
+								$from(resDupls).equals("n", false).select((v) => {
+									nattrmon.setNotified(v.t, parent.instanceId);
+								});
+
+								/*if (isDef(this.alertUntilBySimilarity) && this.alertUntilBySimilarity > 0) {
+								 	if (($from(resDupls).equals("n", true).count() + 1) >= this.alertUntilBySimilarity) {
+										nattrmon.setNotified(owarns[i][j].title, this.instanceId);
+									}
+								}*/
+							}
 
 							if (!nattrmon.isNotified(owarns[i][j].title, this.instanceId) && 
-								this.warnTypes.indexOf(owarns[i][j].level) >= 0)
+								this.warnTypes.indexOf(owarns[i][j].level) >= 0) {
+									list.push(owarns[i][j]);
 									nattrmon.setNotified(owarns[i][j].title, this.instanceId);
-
-							count++;
+									count++;
+							} 
 						}
 					}
 				}
