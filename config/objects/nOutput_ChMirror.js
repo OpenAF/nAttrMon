a4f0c0512a8ede483c1a25a9bef1881a567a21bf
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2021-10-04 21:20:55 +0000

    +nOutput_ChMirror

---
+++ config/objects/nOutput_ChMirror.js
@@ -0,0 +1,43 @@
+// Author: Nuno Aguiar
+
+/**
+ * <odoc>
+ * <key>nattrmon.nOutput_ChMirror(aMap)</key>
+ * 
+ * </odoc>
+ */
+ var nOutput_ChMirror = function(aMap) {
+    if (getVersion() < "20210923") throw "nOutput_ChMirror is only supported starting on OpenAF version 20210923";
+
+    if (isObject(aMap)) {
+        this.params = aMap;
+    } else {
+        this.params = {};
+    }
+    
+    this.channels = _$(aMap.channels, "channels").isMap().default({});
+
+    var chList = $ch().list();
+    Object.keys(this.channels).forEach(ch => {
+        try {
+            _$(this.channels[ch].mirrorFrom, "mirrorFrom").isString().oneOf(chList).$_();
+            if (chList.indexOf(ch) < 0) {
+                log("Creating channel '" + ch + "' for mirror...");
+                var op = __nam_getSec(this.channels[ch]);
+                $ch(ch).create(__, op.type, op.options);
+            }
+
+            $ch(this.channels[ch].mirrorFrom).subscribe(ow.ch.utils.getMirrorSubscriber(ch));
+            log("Mirroring '" + this.channels[ch].mirrorFrom + "' to '" + ch + "'"); 
+        } catch(e) {
+            logErr("ChMirror | Channel '"+ ch + "' | " + String(e));
+        }
+    });
+
+    nOutput.call(this, this.output);
+};
+inherit(nOutput_ChMirror, nOutput);
+
+nOutput_ChMirror.prototype.output = function(scope, args) {
+
+};
