2e39aafb79b7d38ac4f788cad5cfeda7fd7958ca
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2021-10-05 03:57:25 +0000

    +nInput_nAttrMon

---
+++ config/objects/nInput_nAttrMon.js
@@ -0,0 +1,55 @@
+// Author: Nuno Aguiar
+
+/**
+ * <odoc>
+ * <key>nattrmon.nInput_nAttrMon(aMap)</key>
+ * Provides internal nAttrMon monitoring.
+ * On aMap expects:\
+ * \
+ *    - attrTemplate (String) The attribute template where to store the result (use variable name for sub attributes).\
+ * \
+ * </odoc>
+ */
+ var nInput_nAttrMon = function(aMap) {
+    if (isObject(aMap)) {
+        this.params = aMap;
+    } else {
+        this.params = {};
+    }
+
+    if (isUnDef(this.params.attrTemplate)) this.params.attrTemplate = "nAttrMon/{{name}}";
+
+    nInput.call(this, this.input);
+};
+inherit(nInput_nAttrMon, nInput);
+
+nInput_nAttrMon.prototype.input = function(scope, args) {
+    var ret = {};
+
+    ow.loadMetrics();
+    var d = ow.metrics.getAll();
+
+    var nam, namps, thr;
+
+    if (isDef(d.mem)) {
+        ret[templify(this.params.attrTemplate, { name: "Memory" })] = d.mem;
+    }
+
+    if (isDef(d.nattrmon)) {
+        nam = d.nattrmon;
+        namps = clone(d.nattrmon.poolsStats.poolsStats);
+        delete nam.poolsStats;
+
+        ret[templify(this.params.attrTemplate, { name: "nAttrMon" })] = nam;
+        ret[templify(this.params.attrTemplate, { name: "PoolsStats" })] = namps;
+    }
+
+    if (isDef(d.threads)) {
+        thr = d.threads;
+        delete thr.list;
+
+        ret[templify(this.params.attrTemplate, { name: "Threads" })] = thr;
+    }
+
+    return ret;
+};
