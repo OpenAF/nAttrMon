4a0923d38c60b793c8488a0a0f7548339960460c
abrizida <33725785+abrizida@users.noreply.github.com>
2026-04-30 11:28:24 +0100

    Add condition to push memory array to result

--- config/objects/nInput_RAIDGCMemory.js
+++ config/objects/nInput_RAIDGCMemory.js
@@ -114,7 +114,10 @@ nInput_RAIDGCMemory.prototype.input = function(scope, args) {
 		}
 		arr.push(this.__getMemory(this.params.keys[i], extra));
 	}
-
-	res[templify(this.params.attrTemplate)] = arr;
+	
+    if (arr.length > 0) { 
+	   res[templify(this.params.attrTemplate)] = arr;
+	}
+	
 	return res;
 }
